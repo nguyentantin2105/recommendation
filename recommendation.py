77d91d76fc29493fcfa64214b9769554be473562
nguyentantin2105 <93tinndu1314@gmail.com>
2021-08-02 02:02:16 +0700

    update code

--- recommendation.py
+++ recommendation.py
@@ -11,26 +11,20 @@ import time
 import threading
 import csv
 
-user = input("Nhập vào id người dùng: ")
-
-def write_csv(data):
-    f = open('./new_user.csv', 'w')
-    writer = csv.writer(f)
-    writer.writerow('productRecommend')
-    writer.writerow(data)
-    f.close()
-
-def recommend():
-    threading.Timer(5, recommend).start()
-    df = pd.read_csv('./out.csv',)
+def recommend(userid):
+    threading.Timer(900, recommend).start()
+    list_recommend = []
+    df = pd.read_csv('./out.csv')
     df = df.fillna(0)
     popular_products = pd.DataFrame(df.groupby('productRate')['rate'].count())
     most_popular = popular_products.sort_values('rate', ascending=False)
     Recommend_for_new_user = most_popular.head(10)
     Recommend_for_new_user.to_csv('new_user.csv')
-
+    
+    df_new = pd.read_csv('./new_user.csv')
+    list_recommend_to_new_user = list(df_new["productRate"].values)
+    
     ratings_utility_matrix = df.pivot_table(values='rate', index='buyerRate', columns='productRate', fill_value=0)
-
     X = ratings_utility_matrix.T
 
     df1 = df.drop_duplicates('buyerRate')
@@ -39,18 +33,23 @@ def recommend():
 
     correlation_matrix = np.corrcoef(decomposed_matrix)
 
-    df_buyer = df1[df1['buyerRate'] == user]
+    df_buyer = df1[df1['buyerRate'] == userid]
     product_bought = df_buyer['lastOrder'].values
-
-    product_names = list(X.index)
-    product_index = product_names.index(product_bought)
-
-    correlation_product_ID = correlation_matrix[product_index]
-
-
-    Recommend_for_oldUser = list(X.index[correlation_product_ID > 0.50])
-    Recommend_for_oldUser.remove(product_bought)
-    df_recommend_old_user = pd.DataFrame(Recommend_for_oldUser[0:10])
-    df_recommend_old_user.to_csv('oldUser.csv')
+    print(product_bought)
     
-recommend()
+    if (product_bought != 0):
+        product_names = list(X.index)
+        product_index = product_names.index(product_bought)
+
+        correlation_product_ID = correlation_matrix[product_index]
+
+
+        Recommend_for_oldUser = list(X.index[correlation_product_ID > 0.50])
+        Recommend_for_oldUser.remove(product_bought)
+        list_recommend = Recommend_for_oldUser
+    else:
+        list_recommend =  list_recommend_to_new_user
+    
+    return list_recommend
+    
+# recommend('60817365be8a2f0cba7f7640')
